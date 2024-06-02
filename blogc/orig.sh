a597d52b3e102acc3c956fe52b15f270cc6cab40
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2024-06-02 23:47:24 +0200

    blogc: support both autotools and cmake until we have a cmake release

--- blogc/orig.sh
+++ blogc/orig.sh
@@ -7,6 +7,12 @@ source "${SCRIPT_DIR}/utils.sh"
 
 OUTPUT_DIR="$(realpath "${1}")"
 
-"${SCRIPT_DIR}/orig-cmake-cpack-xz.sh" \
-    "${OUTPUT_DIR}" \
-    "blogc"
+if [[ -f CMakeLists.txt ]]; then
+    "${SCRIPT_DIR}/orig-cmake-cpack-xz.sh" \
+        "${OUTPUT_DIR}" \
+        "blogc"
+else
+    "${SCRIPT_DIR}/orig-autotools-xz.sh" \
+        "${OUTPUT_DIR}" \
+        "blogc"
+fi
